3323401be70781c921a7db848d34d51ff93f2488
relfarizi <rafanzhaniel@gmail.com>
2019-04-09 20:30:01 +0700

    update routes

---
+++ application/controllers/C_Klikfiks.php
@@ -0,0 +1,17 @@
+<?php
+	class C_Klikfiks extends CI_Controller
+	{
+		public function __construct()
+		{
+			parent::__construct();
+			//$this->load->view('Header');
+		}
+
+		public function index()
+		{
+			$this->load->view('header');
+			//$this->load->view('home');
+
+		}
+	}
+?>
---
+++ application/views/header.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand col-lg-6" href="#">Klikfiks</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link col-sm-2" href="#">Tentang Kami <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link col-lg-1" href="#">Track Order</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link col-lg-1" href="#">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled col-lg-1" href="#">FAQs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled col-lg-2" href="#">Hubungi Kami di</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
