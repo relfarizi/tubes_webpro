bbd878cd11b056d6ffddd3ffebe6c8169a690b53
dwinovSW <46889622+dwinovSW@users.noreply.github.com>
2019-04-14 15:32:24 +0700

    Update klix_fiks.sql

--- klix_fiks.sql
+++ klix_fiks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 09 Apr 2019 pada 15.11
+-- Generation Time: 14 Apr 2019 pada 10.31
 -- Versi Server: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -33,6 +33,16 @@ CREATE TABLE `brand` (
   `namaBrand` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `brand`
+--
+
+INSERT INTO `brand` (`idBrand`, `namaBrand`) VALUES
+(1, 'xiaomi'),
+(2, 'samsung'),
+(3, 'apple'),
+(4, 'asus');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +56,22 @@ CREATE TABLE `detailkeluhan` (
   `idKeluhan` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `detailkeluhan`
+--
+
+INSERT INTO `detailkeluhan` (`iddetail`, `idSeries`, `kisaranHarga`, `idKeluhan`) VALUES
+(1, 3, '1700000-2500000', 1),
+(2, 3, '450000-600000', 5),
+(3, 4, '1700000-2500000', 3),
+(4, 4, '1700000-2500000', 4),
+(5, 1, '100000-750000', 2),
+(6, 2, '75000-200000', 5),
+(7, 7, '1700000-2500000', 4),
+(8, 8, '2000000-3000000', 3),
+(9, 5, '100000-750000', 2),
+(10, 6, '350000-1000000', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -55,8 +81,19 @@ CREATE TABLE `detailkeluhan` (
 CREATE TABLE `keluhan` (
   `idKeluhan` int(3) NOT NULL,
   `namaKeluhan` varchar(15) NOT NULL,
-  `keterangan` varchar(50) NOT NULL
+  `keterangan` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `keluhan`
+--
+
+INSERT INTO `keluhan` (`idKeluhan`, `namaKeluhan`, `keterangan`) VALUES
+(1, 'Batrai', 'Kerusakan pada connector,Kerusakan pada fleksibel baterai,Kerusakan pada internal '),
+(2, 'Mati Total', 'Kerusakan internal perangkat,Gangguan IC power,Gangguan IC USB atau IC Charging,Kerusakan pada emmc,Korsleting atau arus pendek'),
+(3, 'Touchscreen', 'Layar retak,Kerusakan layar fleksibel, Gangguan internal mainboard,gangguan software, dan kerusakan penghubung mainboard,Kerusakan pada IC Touchscreen'),
+(4, 'LCD/layar', 'Layar pecah,Pixel mati,Tidak ada tampilan, Kerusakan penghubung atau connector kotor Kerusakan internal perangkat, Gangguan IC power'),
+(5, 'charging', 'Kerusakan baterai, Kerusakan pada penghubung charger, gangguan pada mainboard,Kerusakan pada IC USB');
 
 -- --------------------------------------------------------
 
@@ -83,6 +120,20 @@ CREATE TABLE `series` (
   `idBrand` int(11) NOT NULL,
   `idTipe` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `series`
+--
+
+INSERT INTO `series` (`idSeries`, `namaSeries`, `idBrand`, `idTipe`) VALUES
+(1, 'redmi note 3 ', 1, 1),
+(2, 'redmi note 5', 1, 1),
+(3, 'iphone 6', 3, 1),
+(4, 'iphone 7', 3, 1),
+(5, 'zenfone 2', 4, 1),
+(6, 'zenfone 4', 4, 1),
+(7, 'samsung s6', 2, 1),
+(8, 'samsung s8 edge', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -157,19 +208,19 @@ ALTER TABLE `tipe_device`
 -- AUTO_INCREMENT for table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `idBrand` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `idBrand` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `detailkeluhan`
 --
 ALTER TABLE `detailkeluhan`
-  MODIFY `iddetail` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `iddetail` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `keluhan`
 --
 ALTER TABLE `keluhan`
-  MODIFY `idKeluhan` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `idKeluhan` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -181,7 +232,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `series`
 --
 ALTER TABLE `series`
-  MODIFY `idSeries` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idSeries` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tipe_device`
