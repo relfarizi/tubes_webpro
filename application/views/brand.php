a16315447389046990ab80e44080b2d73dc5bd85
dwinovSW <46889622+dwinovSW@users.noreply.github.com>
2019-04-18 21:34:58 +0700

    Create brand.php

---
+++ application/views/brand.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Homepage</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/home.css">
+</head>
+
+<body>
+<
+  <div class="container ">
+    <h3 class="text-center" style="padding-bottom: 10px">5 Langkah Mudah Service di Klikfix</h3>
+    <table class="container">
+      <tr >
+        <th><img src="assets/costumer_service.png" class="image"></th>
+        <th><img src="assets/pickup.png" class="image" ></th>
+        <th><img src="assets/diagnosa.png" class="image"></th>
+        <th><img src="assets/repair.png" class="image"></th>
+        <th><img src="assets/delivery.png" class="image" style="height: 160%"></th>
+      </tr>
+      <tr class="text-center" style="font-size: 10px; padding-right: 5px; padding-top: 10px ">
+        <td>Langkah 1 : Hubungi kami untuk membuat janji bertemu</td>
+        <td>Langkah 2 : Kurir kami akan mengambil gadget Anda</td>
+        <td>Langkah 3 : Teknisi akan mendiagnosa kerusakan</td>
+        <td>Langkah 4 : Gadget Anda diperbaiki oleh teknisi kami</td>
+        <td>Langkah 5 : Kurir mengantar gadget ke tempat Anda</td>
+      </tr>
+    </table>
+  </div>
+</div>
+
+
+
+
+<div class="container-fluid" style="background-color: blue; padding-top: 50 px; font-size: 12px; ">
+  <div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light container-fluid">
+  <div class="nv-bar" style="text-align: center; ">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link col-lg" href="#"  style="color: white">Tentang Kami </a>
+      </li>
+      <li class="nav-item active" >
+        <a class="nav-link col-lg" href="#" style="color: white">Track Order</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link col-lg" href="#" style="color: white">Blog</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link col-lg" href="#" style="color: white">FAQs</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link col-lg" href="#" style="color: white">Hubungi Kami di</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </div>
+</div>
+</body>
+</html>
