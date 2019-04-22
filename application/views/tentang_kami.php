c8d7062d4ba5cf6e3aeeeef159270b3ffab3e8f3
mhaikala <muhamad2haikal99@gmail.com>
2019-04-22 19:04:35 +0700

    Create tentang_kami.php

---
+++ application/views/tentang_kami.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tentang Kamii</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/tentangKami.css">
+</head>
+
+<body>
+    <div clas="container-fluid" style="background: linear-gradient(to bottom, #33cccc 1%, #bce0cd 100%) ; color: white;">
+        <div>
+        <table class="container">
+            <tr >
+                <th><img src="assets/logo.png" class="image" style="max-width: 100%;
+                height: auto;"></th>
+            </tr>
+        </table>
+        </div>
+        <div class="container">
+            <h3 class="text-center" style="padding-top:50px; ">
+                <div>Jasa Service Handphone & Laptop Panggilan</div>
+                <div>Terpecaya di Jabodetabek & Bandung</div>
+            </h3>
+            <h6 class="text-center" style="padding-top: 25px;">
+                <div>Klikfix merupakan jasa service handphone dan laptop terpercaya.</div>
+                <div>Konsumen tidak harus datang ke toko dan menganteri panjang untuk service handphone dan laptop.</div>
+                <div>Hanya dengan klik, kurir akan segera datang dan mengambil gadget untuk di diagnosa oleh fixxer kami.</div>
+            </h6>
+            <a href="/#">
+            <button class=".btn-primary">PERBAIKI SEKARANG</button>
+        </a>
+    </div>
+</body>
+</html>
