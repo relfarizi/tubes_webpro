b1f997ffcca88008f02022aa996431d5e5181a8f
mhaikala <muhamad2haikal99@gmail.com>
2019-04-23 17:41:05 +0700

    tentang kami

--- application/views/tentang_kami.php
+++ application/views/tentang_kami.php
@@ -6,7 +6,7 @@
 </head>
 
 <body>
-    <div class="container-fluid" style="background-color: blue; color: white;padding-top: 30px;padding-bottom: 30px">
+    <div class="container-fluid" style="background-color: blue; color: white;padding-top: 30px;padding-bottom: 50px">
         <div>
         <table class="container">
             <tr >
@@ -20,8 +20,13 @@
                 <div>Jasa Service Handphone & Laptop Panggilan</div>
                 <div>Terpecaya di Jabodetabek & Bandung</div>
             </h3>
+            <div id="play-button" class="col-md-12 text-center" ng-click="open()">
+                <a href="/#">
+                    <img class="play-button-image" src="<?php echo base_url("assets/playbutton.svg")?>">
+                </a>
+            </div>
             <h6 class="text-center" style="padding-top: 25px;">
-                <div>Klikfix merupakan jasa service handphone dan laptop terpercaya.</div>
+                <div>Klixfiks merupakan jasa service handphone dan laptop terpercaya.</div>
                 <div>Konsumen tidak harus datang ke toko dan menganteri panjang untuk service handphone dan laptop.</div>
                 <div>Hanya dengan klik, kurir akan segera datang dan mengambil gadget untuk di diagnosa oleh fixxer kami.</div>
             </h6>
@@ -32,10 +37,10 @@
         </center>
     </div>
 
-    <div style="background-color: white; padding-bottom: 50px; padding-top: 25px ;;
+    <div style="background-color: white; padding-bottom: 50px; padding-top: 30px ;;
     ">
-        <div class="container ">
-            <h3 class="text-center" style="padding-bottom: 10px">5 Langkah Mudah Service di Klikfix</h3>
+        <div class="container-fluid ">
+            <h3 class="text-center" style="padding-bottom: 10px; color: black">5 Langkah Mudah Service di Klikfix</h3>
             <table class="container">
                 <tr >
                     <th><img src="<?php echo base_url("assets/costumer_service.png")?>" class="image"></th>
@@ -55,16 +60,15 @@
         </div>
     </div>
 
-    <div style="
-     padding-bottom: 50px; padding-top: 25px;background-image : url'<?php echo base_url('assets/work.png')?>';color: white">
-        <div class="container ">
+    <div class="container-fluid" style="padding-bottom: 50px; padding-top: 25px;background-color : #33cccc ;color: white;">
+        <div>
             <h3 class="text-center" style="padding-bottom: 10px">Keunggulan Klikfix</h3>
             <table class="container">
                 <tr>
                     <th><img src="<?php echo base_url("assets/money.png")?>" class="image" style="height : 150%"></th>
                     <th><img src="<?php echo base_url("assets/pickup.png")?>" class="image"></th>
                     <th><img src="<?php echo base_url("assets/garansi.png")?>" class="image"></th>
-                    th><img src="<?php echo base_url("assets/teknisi.png")?>" class="image"></th>
+                    <th><img src="<?php echo base_url("assets/teknisi.png")?>" class="image"></th>
                 </tr>
                 <tr class="text-center" style="font-size: 10px; padding-right: 5px; color: white">
                     <td>
@@ -91,7 +95,7 @@
     <div style="background-color: white; padding-bottom: 50px; padding-top: 25px ;;
     ">
         <div class="container ">
-            <h3 class="text-center" style="padding-bottom: 5px">Testimoni</h3>
+            <h3 class="text-center" style="padding-bottom: 5px; color: black">Testimoni</h3>
         </div>
 
         <div class="container">
@@ -99,14 +103,14 @@
             <tr class="text-center" style="font-size: 10px; padding-right: 5px; padding-top: 10px;">
                 <td>
                     <div>
-                        <h4 style="  color: white; padding-top: 40px">Adika Muhamad</h4>
+                        <h4 style="  color: black; padding-top: 40px">Adika Muhamad</h4>
                         <div>Sejauh ini bagus pelayanannya,</div>
                         <div>dari CS dari teknisinya selalu respect</div>
                     </div>
                 </td>
                 <td>
                     <div>
-                        <h4 style="  color: white; padding-top: 40px">Khakans</h4>
+                        <h4 style="  color: black; padding-top: 40px">Khakans</h4>
                         <div>Samsung S11 gua rusak lcdnya,</div>
                         <div>terus gua bawa ke klixfiks.</div>
                         <div>pengerjaannya ga nyampe 2 hari sii</div>
@@ -115,15 +119,29 @@
                 </td>
                 <td>
                     <div>
-                        <h4 style="  color: white; padding-top: 40px">Bebby Fey</h4>
+                        <h4 style="  color: black; padding-top: 40px">Bebby Fey</h4>
                         <div>Kurirnya dateng tepat waktu</div>
                         <div>sesuai dengan perjanjian dan mengembalikan hp saya</div>
                         <div>Pokoknya maah mangstaap poll</div>
                     </div>
                 </td>
+                <td>
+                    <div>
+                        <h4 style="  color: black; padding-top: 40px">Adamskay</h4>
+                        <div>Thanks Klikfix...</div>
+                        <div>Laptop jauh lebih ringan dan edit audio ringan</div>
+                        <div>Makasih klikfix muuach</div>
+                    </div>
+                </td>
                 </td>
             </tr>
         </table>
+        </div>
+    </div>
+
+    <div class="container-fluid" style="background-color: grey; color: white;padding-top: 30px;padding-bottom: 50px">
+        <div class="container ">
+            <h3 class="text-center" style="padding-bottom: 5px; color: black">Kontak Kami</h3>
         </div>
     </div>
 
